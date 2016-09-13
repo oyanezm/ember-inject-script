4f02c5872c2282556ecd32d24a2449dbb70d98f7
oyanezm <oyanezm@gmail.com>
2016-09-13 20:18:43 -0300

    add reject support

--- addon/utils/inject-script.js
+++ addon/utils/inject-script.js
@@ -1,12 +1,18 @@
 import Ember from 'ember';
 
 export default function injectScript(src) {
-  return new Ember.RSVP.Promise(function(resolve) {
+  return new Ember.RSVP.Promise(function(resolve,reject) {
     var script    = document.createElement('script');
     script.type   = 'text/javascript';
     script.async  = true;
     script.src    = src;
     script.onload = function() { resolve(); };
-    document.getElementsByTagName('head')[0].appendChild(script);
+
+    try{
+      document.getElementsByTagName('head')[0].appendChild(script);
+    }catch(error){
+      reject();
+    }
+
   });
 }
